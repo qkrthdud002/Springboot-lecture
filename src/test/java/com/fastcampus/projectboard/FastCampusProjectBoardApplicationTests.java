fa2694c6f29c574418e0808c14cd4095bf15b230
qkrthdud002 <qkrthdud002@gmail.com>
2022-09-03 23:48:41 +0900

    0903

--- src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
+++ src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
@@ -1,13 +1,19 @@
 package com.fastcampus.projectboard;
 
+import com.fastcampus.projectboard.domain.Article;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class FastCampusProjectBoardApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
--- src/test/java/com/fastcampus/projectboard/reppository/JpaRepository.java
+++ src/test/java/com/fastcampus/projectboard/reppository/JpaRepository.java
@@ -4,18 +4,21 @@ import com.fastcampus.projectboard.config.JpaConfig;
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.repository.ArticleCommentRepository;
 import com.fastcampus.projectboard.repository.ArticleRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.hamcrest.Matchers;
 
 import java.util.List;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("JPA 연결 테스트")
-@Import(JpaConfig.class)
+@Import(JpaConfig.class) // auditing (자동으로 연결해주는 기능).
 @DataJpaTest
 class JpaRepositoryTest {
 
@@ -31,6 +34,7 @@ class JpaRepositoryTest {
         this.articleCommentRepository = articleCommentRepository;
     }
 
+//     .hasSize() 부분에서 오류남.
     @DisplayName("select 테스트")
     @Test
     void givenTestData_whenSelecting_thenWorkFine() {
@@ -40,9 +44,28 @@ class JpaRepositoryTest {
         List<Article> articles = articleRepository.findAll();
 
         // Then
+
         assertThat(articles)
                 .isNotNull()
                 .hasSize(0);
+
+    }
+
+    @DisplayName("insert 테스트")
+    @Test
+    void givenTestData_whenInserting_thenWorksFine() {
+        // Given
+        long previousCount = articleRepository.count();
+
+        // When
+        Article savedArticle = articleRepository.save(Article.of("new article", "new content", "#spring"));
+
+
+        // Then
+        assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
+
+
+
     }
 
 }
